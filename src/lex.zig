7d97e4d73987204344ff79957711d0a34459617e
Faerryn <alexandre.liao@gmail.com>
2020-07-17 14:16:25 -0400

    Lexer

---
+++ src/lex.zig
@@ -0,0 +1,203 @@
+const std = @import("std");
+
+pub const Token = struct {
+    id: Id,
+    start: usize,
+    end: usize,
+
+    pub const Id = enum {
+        invalid,
+        identifier,
+        string_literal,
+        integer_literal,
+        float_literal,
+        line_comment,
+        open_parenthesis,
+        close_parenthesis,
+        quote,
+    };
+};
+
+pub const Lexer = struct {
+    buffer: []const u8,
+    index: usize,
+
+    pub fn init(buffer: []const u8) Lexer {
+        return Lexer{
+            .buffer = buffer,
+            .index = 0,
+        };
+    }
+
+    const State = enum {
+        start,
+        integer_literal,
+        float_fraction,
+        string_literal,
+        string_literal_backslash,
+        identifier,
+        line_comment,
+        sign,
+        period,
+    };
+
+    pub fn next(self: *Lexer) ?Token {
+        const start_index = self.index;
+        var state = State.start;
+        var eof = true;
+        var result = Token{
+            .id = .invalid,
+            .start = start_index,
+            .end = undefined,
+        };
+        while (self.index < self.buffer.len) : (self.index += 1) {
+            const c = self.buffer[self.index];
+            switch (state) {
+                .start => switch (c) {
+                    ' ', '\t', '\r', '\n' => {
+                        result.start = self.index + 1;
+                    },
+                    '"' => {
+                        eof = false;
+                        state = .string_literal;
+                    },
+                    '(' => {
+                        eof = false;
+                        result.id = .open_parenthesis;
+                        self.index += 1;
+                        break;
+                    },
+                    ')' => {
+                        eof = false;
+                        result.id = .close_parenthesis;
+                        self.index += 1;
+                        break;
+                    },
+                    '\'' => {
+                        eof = false;
+                        result.id = .quote;
+                        self.index += 1;
+                        break;
+                    },
+                    '0'...'9' => {
+                        eof = false;
+                        state = .integer_literal;
+                        result.id = .integer_literal;
+                    },
+                    '+', '-' => {
+                        eof = false;
+                        state = .sign;
+                    },
+                    '.' => {
+                        eof = false;
+                        state = .period;
+                    },
+                    ';' => {
+                        eof = false;
+                        state = .line_comment;
+                        result.id = .line_comment;
+                    },
+                    else => {
+                        eof = false;
+                        state = .identifier;
+                        result.id = .identifier;
+                    },
+                },
+                .string_literal => {
+                    switch (c) {
+                        '\\' => {
+                            state = .string_literal_backslash;
+                        },
+                        '"' => {
+                            result.id = .string_literal;
+                            self.index += 1;
+                            break;
+                        },
+                        else => {},
+                    }
+                },
+                .string_literal_backslash => {
+                    state = .string_literal;
+                },
+                .integer_literal => {
+                    switch (c) {
+                        '0'...'9' => {},
+                        '.' => {
+                            state = .float_fraction;
+                            result.id = .float_literal;
+                        },
+                        ' ', '\t', '\r', '\n', '(', ')', ';' => break,
+                        else => {
+                            state = .identifier;
+                            result.id = .identifier;
+                        },
+                    }
+                },
+                .float_fraction => {
+                    switch (c) {
+                        '0'...'9' => {},
+                        ' ', '\t', '\r', '\n', '(', ')', ';' => break,
+                        else => {
+                            state = .identifier;
+                            result.id = .identifier;
+                        },
+                    }
+                },
+                .sign => {
+                    switch (c) {
+                        '0'...'9' => {
+                            state = .integer_literal;
+                            result.id = .integer_literal;
+                        },
+                        '.' => {
+                            state = .float_fraction;
+                            result.id = .float_literal;
+                        },
+                        ' ', '\t', '\r', '\n', '(', ')', ';' => {
+                            result.id = .identifier;
+                            break;
+                        },
+                        else => {
+                            state = .identifier;
+                            result.id = .identifier;
+                        },
+                    }
+                },
+                .period => {
+                    switch (c) {
+                        '0'...'9' => {
+                            state = .float_fraction;
+                            result.id = .float_literal;
+                        },
+                        ' ', '\t', '\r', '\n', '(', ')', ';' => {
+                            result.id = .identifier;
+                            break;
+                        },
+                        else => {
+                            state = .identifier;
+                            result.id = .identifier;
+                        },
+                    }
+                },
+                .line_comment => {
+                    switch (c) {
+                        '\r', '\n' => break,
+                        else => {},
+                    }
+                },
+                .identifier => {
+                    switch (c) {
+                        ' ', '\t', '\r', '\n', '(', ')', ';' => break,
+                        else => {},
+                    }
+                },
+            }
+        }
+        if (eof) {
+            return null;
+        } else {
+            result.end = self.index;
+            return result;
+        }
+    }
+};
---
+++ src/main.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const lex = @import("lex.zig");
+const Lexer = lex.Lexer;
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
+
+pub fn main() anyerror!void {
+    var line = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer line.deinit();
+
+    while (true) {
+        try stdout.print(" (fey lisp) ", .{});
+
+        line.shrink(0); // clear the line
+        stdin.readUntilDelimiterArrayList(&line, '\n', std.math.maxInt(usize)) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => |e| return e,
+        };
+        var lexer = Lexer.init(line.items);
+        while (lexer.next()) |token| {
+            try stdout.print(" {}: {}\n", .{ token.id, line.items[token.start..token.end] });
+        }
+    }
+}
