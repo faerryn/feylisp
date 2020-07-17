d262f79b12d91d62a272286b52480400b22f60f5
Faerryn <alexandre.liao@gmail.com>
2020-07-17 16:51:32 -0400

    Superior REPL

--- src/main.zig
+++ src/main.zig
@@ -4,20 +4,41 @@ const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
 
 pub fn main() anyerror!void {
-    var line = std.ArrayList(u8).init(std.heap.page_allocator);
-    defer line.deinit();
+    var source = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer source.deinit();
+    var source_index: usize = 0;
 
-    while (true) {
-        try stdout.print(" (fey lisp) ", .{});
+    try stdout.print(" (fey lisp) ", .{});
 
-        line.shrink(0); // clear the line
-        stdin.readUntilDelimiterArrayList(&line, '\n', std.math.maxInt(usize)) catch |err| switch (err) {
-            error.EndOfStream => break,
-            else => |e| return e,
-        };
-        var lexer = Lexer.init(line.items);
+    repl_loop: while (true) {
+        var parens: usize = 0;
+        var newline = false;
+        while (parens > 0 or !newline) {
+            const c = stdin.readByte() catch |err| switch (err) {
+                error.EndOfStream => break :repl_loop,
+                else => return err,
+            };
+            switch (c) {
+                '(' => parens += 1,
+                ')' => parens -= 1,
+                '\n' => {
+                    newline = true;
+                    if (parens > 0) {
+                        try stdout.print("            ", .{});
+                    } else {
+                        try stdout.print(" (fey lisp) ", .{});
+                    }
+                },
+                else => {},
+            }
+            try source.append(c);
+        }
+
+        const line = source.items[source_index..];
+        source_index = source.items.len;
+        var lexer = Lexer.init(line);
         while (lexer.next()) |token| {
-            try stdout.print(" {}: {}\n", .{ token.id, line.items[token.start..token.end] });
+            try stdout.print(" {}: {}\n", .{ token.id, line[token.start..token.end] });
         }
     }
 }
