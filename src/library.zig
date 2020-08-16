20e7be18c4e844b50ca57f059c6b2fa7162952bd
Faerryn <alexandre.liao@gmail.com>
2020-08-16 00:17:50 -0400

    Lists now kinda work...
    TODO ensure we avoid stale pointers when dealing with lists

--- src/library.zig
+++ src/library.zig
@@ -20,9 +20,12 @@ pub fn initCore(allocator: *std.mem.Allocator) !interpret.Interpreter {
     try core.scope.put("let", try core.store(interpret.Expr{ .native_macro = @ptrToInt(let) }));
     try core.scope.put("func", try core.store(interpret.Expr{ .native_macro = @ptrToInt(func) }));
     try core.scope.put("macro", try core.store(interpret.Expr{ .native_macro = @ptrToInt(macro) }));
-    try core.scope.put("list", try core.store(interpret.Expr{ .native_func = @ptrToInt(list) }));
     try core.scope.put("if", try core.store(interpret.Expr{ .native_macro = @ptrToInt(@"if") }));
     try core.scope.put("while", try core.store(interpret.Expr{ .native_macro = @ptrToInt(@"while") }));
+    try core.scope.put("list", try core.store(interpret.Expr{ .native_func = @ptrToInt(list) }));
+    try core.scope.put("len", try core.store(interpret.Expr{ .native_func = @ptrToInt(len) }));
+    try core.scope.put("at", try core.store(interpret.Expr{ .native_func = @ptrToInt(at) }));
+    try core.scope.put("push", try core.store(interpret.Expr{ .native_func = @ptrToInt(push) }));
     return core;
 }
 
@@ -56,7 +59,7 @@ fn ComparisonAccumulator(comp: Comparison) type {
         fn accumulate(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
             const first = switch (args[0].*) {
                 .number => |number| number,
-                else => return error.ComparisonArgumentNotANumber,
+                else => return error.ComparisonNotANumber,
             };
             for (args[1..]) |branch| switch (branch.*) {
                 .number => |number| if (!switch (comp) {
@@ -66,16 +69,16 @@ fn ComparisonAccumulator(comp: Comparison) type {
                     .gteq => first >= number,
                     .lt => first < number,
                     .lteq => first <= number,
-                }) return interpreter.store(.{ .list = std.ArrayList(*interpret.Expr).init(interpreter.allocator) }),
-                else => return error.ComparisonArgumentNotANumber,
+                }) return try interpreter.store(.{ .list = std.ArrayList(*interpret.Expr).init(interpreter.allocator) }),
+                else => return error.ComparisonNotANumber,
             };
-            return interpreter.store(.{ .number = 1.0 });
+            return try interpreter.store(.{ .number = 1.0 });
         }
     };
 }
 
 fn print(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
-    if (args.len == 0) return error.PrintNoArguments;
+    if (args.len == 0) return error.PrintInvalidArguments;
     for (args) |arg| {
         switch (arg.*) {
             .string => |string| try stdout.print("{}", .{string.items}),
@@ -86,66 +89,55 @@ fn print(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpr
 }
 
 fn let(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
-    if (args.len != 2) return error.LetInvalidArgumentsLength;
+    if (args.len != 2) return error.LetInvalidArguments;
     switch (args[0].*) {
         .identifier => |identifier| {
             const value = try interpreter.eval(args[1]);
             try interpreter.scope.put(identifier.items, value);
             return value;
         },
-        else => return error.LetInvalidIdentifier,
+        else => return error.LetNoIdentifier,
     }
 }
 
 fn func(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
-    if (args.len < 2) return error.FuncInvalidArgumentsLength;
-    const params = switch (args[0].*) {
-        .list => |params| blk: {
-            var params_copy = try std.ArrayList(*interpret.Expr).initCapacity(interpreter.allocator, params.items.len);
-            errdefer params.deinit();
-            for (params.items) |param| switch (param.*) {
-                .identifier => |_| try params_copy.append(param),
-                else => return error.FuncInvalidParameter,
-            };
-            break :blk params_copy;
-        },
-        else => return error.FuncInvalidParametersList,
-    };
+    if (args.len < 2) return error.FuncInvalidArguments;
+    if (args[0].* != .list) return error.FuncNoParameters;
+    var params = try std.ArrayList(*interpret.Expr).initCapacity(interpreter.allocator, args[0].list.items.len);
+    errdefer params.deinit();
+    for (args[0].list.items) |param| {
+        if (param.* == .identifier) {
+            try params.append(param);
+        } else {
+            return error.FuncInvalidParameter;
+        }
+    }
     var body = try std.ArrayList(*interpret.Expr).initCapacity(interpreter.allocator, args.len - 1);
     for (args[1..]) |expr| try body.append(expr);
-    return interpreter.store(interpret.Expr{ .func = .{ .params = params, .body = body } });
+    return try interpreter.store(interpret.Expr{ .func = .{ .params = params, .body = body } });
 }
 
 fn macro(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
-    if (args.len < 2) return error.MacroInvalidArgumentsLength;
-    const params = switch (args[0].*) {
-        .list => |params| blk: {
-            var params_copy = try std.ArrayList(*interpret.Expr).initCapacity(interpreter.allocator, params.items.len);
-            errdefer params.deinit();
-            for (params.items) |param| switch (param.*) {
-                .identifier => |_| try params_copy.append(param),
-                else => return error.MacroInvalidParameter,
-            };
-            break :blk params_copy;
-        },
-        else => return error.MacroInvalidParametersList,
-    };
+    if (args.len < 2) return error.MacroInvalidArguments;
+    if (args[0].* != .list) return error.FuncNoParameters;
+    var params = try std.ArrayList(*interpret.Expr).initCapacity(interpreter.allocator, args[0].list.items.len);
+    errdefer params.deinit();
+    for (args[0].list.items) |param| {
+        if (param.* == .identifier) {
+            try params.append(param);
+        } else {
+            return error.FuncInvalidParameter;
+        }
+    }
     var body = try std.ArrayList(*interpret.Expr).initCapacity(interpreter.allocator, args.len - 1);
     for (args[1..]) |expr| try body.append(expr);
-    return interpreter.store(interpret.Expr{ .macro = .{ .params = params, .body = body } });
-}
-
-fn list(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
-    var exprs = try std.ArrayList(*interpret.Expr).initCapacity(interpreter.allocator, args.len);
-    errdefer exprs.deinit();
-    for (args) |arg| try exprs.append(arg);
-    return interpreter.store(interpret.Expr{ .list = exprs });
+    return try interpreter.store(interpret.Expr{ .macro = .{ .params = params, .body = body } });
 }
 
 fn @"if"(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
-    if (args.len < 3) return error.IfInvalidArgumentsLength;
+    if (args.len < 3) return error.IfInvalidArguments;
     const cond = switch ((try interpreter.eval(args[0])).*) {
-        .list => |cond_expr| cond_expr.items.len > 0,
+        .list => |cond| cond.items.len > 0,
         else => true,
     };
     if (cond) {
@@ -159,12 +151,39 @@ fn @"if"(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpr
 }
 
 fn @"while"(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
-    if (args.len < 2) return error.IfInvalidArgumentsLength;
+    if (args.len < 2) return error.IfInvalidArguments;
     while (switch ((try interpreter.eval(args[0])).*) {
-        .list => |cond_expr| cond_expr.items.len > 0,
+        .list => |cond| cond.items.len > 0,
         else => true,
     }) {
         for (args[1..]) |branch| _ = try interpreter.eval(branch);
     }
-    return interpreter.store(.{ .list = std.ArrayList(*interpret.Expr).init(interpreter.allocator) });
+    return try interpreter.store(.{ .list = std.ArrayList(*interpret.Expr).init(interpreter.allocator) });
+}
+
+fn list(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
+    var exprs = try std.ArrayList(*interpret.Expr).initCapacity(interpreter.allocator, args.len);
+    errdefer exprs.deinit();
+    for (args) |arg| try exprs.append(arg);
+    return try interpreter.store(interpret.Expr{ .list = exprs });
+}
+
+fn len(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
+    if (args.len != 1) return error.LenInvalidArguments;
+    if (args[0].* != .list) return error.PushNotAList;
+    return try interpreter.store(.{ .number = @intToFloat(f64, args[0].list.items.len) });
+}
+
+fn at(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
+    if (args.len != 2) return error.AtInvalidArguments;
+    if (args[0].* != .list) return error.PushNotAList;
+    if (args[1].* != .number) return error.PushNotANumber;
+    return args[0].list.items[@floatToInt(usize, args[1].number)];
+}
+
+fn push(interpreter: *interpret.Interpreter, args: []*interpret.Expr) !*interpret.Expr {
+    if (args.len < 2) return error.PushInvalidArguments;
+    if (args[0].* != .list) return error.PushNotAList;
+    for (args[1..]) |branch| try args[0].list.append(branch);
+    return args[args.len - 1];
 }
