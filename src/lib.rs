ee5d962d40d0dd2e5de8169b8469320f90e8e10e
Faerryn <alexandre.liao@gmail.com>
2022-03-20 21:12:08 -0400

    move most things into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -688,3 +688,68 @@ pub fn eval(
         _ => Ok((Some(expr), env)),
     }
 }
+
+#[derive(Debug)]
+pub enum EvalSrcError {
+    Lex(LexError),
+    Parse(ParseError),
+    Eval(EvalError),
+}
+
+impl std::fmt::Display for EvalSrcError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for EvalSrcError {}
+
+pub fn eval_src(
+    src: &str,
+    mut env: Environment,
+) -> Result<(Vec<Expression>, Environment), EvalSrcError> {
+    let exprs = parse(lex(src).map_err(EvalSrcError::Lex)?).map_err(EvalSrcError::Parse)?;
+    let mut result = vec![];
+    result.reserve(exprs.len());
+
+    for expr in exprs {
+        let (expr, new_env) = eval(expr, env).map_err(EvalSrcError::Eval)?;
+        if let Some(expr) = expr {
+            result.push(expr);
+        }
+        env = new_env;
+    }
+
+    Ok((result, env))
+}
+
+pub fn repl(mut env: Environment) -> Result<Environment, Box<dyn std::error::Error>> {
+    use std::io::prelude::*;
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+
+    let mut src = String::new();
+    let mut line = String::new();
+
+    print!("> ");
+    stdout.flush()?;
+
+    while stdin.read_line(&mut line)? > 0 {
+        src.push_str(&line);
+        line.clear();
+        if let Some(exprs) = lex(&src).ok().and_then(|src| parse(src).ok()) {
+            src.clear();
+            for expr in exprs {
+                let (expr, new_env) = eval(expr, env)?;
+                env = new_env;
+                if let Some(expr) = expr {
+                    println!("{}", expr);
+                }
+            }
+            print!("> ");
+            stdout.flush()?;
+        }
+    }
+
+    Ok(env)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use feylisp::{eval, lex, parse, Environment, EvalError, Expression, LexError, ParseError};
+use feylisp::{eval_src, repl, Environment};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut env = Environment::default();
@@ -19,7 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::*;
+    use feylisp::{eval_src, Environment, Expression};
 
     #[test]
     fn factorial() {
@@ -52,69 +52,4 @@ mod tests {
         };
         assert!(matches!(result, Ok(([Expression::Number(55)], _))));
     }
-}
-
-fn repl(mut env: Environment) -> Result<Environment, Box<dyn std::error::Error>> {
-    use std::io::prelude::*;
-    let stdin = std::io::stdin();
-    let mut stdout = std::io::stdout();
-
-    let mut src = String::new();
-    let mut line = String::new();
-
-    print!("> ");
-    stdout.flush()?;
-
-    while stdin.read_line(&mut line)? > 0 {
-        src.push_str(&line);
-        line.clear();
-        if let Some(exprs) = lex(&src).ok().and_then(|src| parse(src).ok()) {
-            src.clear();
-            for expr in exprs {
-                let (expr, new_env) = eval(expr, env)?;
-                env = new_env;
-                if let Some(expr) = expr {
-                    println!("{}", expr);
-                }
-            }
-            print!("> ");
-            stdout.flush()?;
-        }
-    }
-
-    Ok(env)
-}
-
-#[derive(Debug)]
-enum EvalSrcError {
-    Lex(LexError),
-    Parse(ParseError),
-    Eval(EvalError),
-}
-
-impl std::fmt::Display for EvalSrcError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl std::error::Error for EvalSrcError {}
-
-fn eval_src(
-    src: &str,
-    mut env: Environment,
-) -> Result<(Vec<Expression>, Environment), EvalSrcError> {
-    let exprs = parse(lex(src).map_err(EvalSrcError::Lex)?).map_err(EvalSrcError::Parse)?;
-    let mut result = vec![];
-    result.reserve(exprs.len());
-
-    for expr in exprs {
-        let (expr, new_env) = eval(expr, env).map_err(EvalSrcError::Eval)?;
-        if let Some(expr) = expr {
-            result.push(expr);
-        }
-        env = new_env;
-    }
-
-    Ok((result, env))
 }
